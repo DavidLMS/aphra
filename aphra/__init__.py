31bea4b7e87be0cc0874c54f6b4c70de6f157540
David <17435126+DavidLMS@users.noreply.github.com>
2024-08-16 11:38:26 +0200

    Modify __init__ for docs
    Modify por add documentation of all submodules

--- aphra/__init__.py
+++ aphra/__init__.py
@@ -5,5 +5,8 @@ This module exposes the translate function from the translate module.
 """
 
 from .translate import translate
+from . import llm_client
+from . import prompts
+from . import parsers
 
-__all__ = ['translate']
+__all__ = ['translate', 'llm_client', 'prompts', 'parsers']
